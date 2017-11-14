67172853d49b3ca10d02b27034ede1bb1ec3f460
exsesx <exsesx@gmail.com>
2017-11-14 18:43:58 +0200

    socket base implementation

--- chat-frontend/src/components/TestComponent.js
+++ chat-frontend/src/components/TestComponent.js
@@ -1,6 +1,14 @@
 import React, {Component} from 'react';
+import socket from 'utils/socket';
 
 export default class TestComponent extends Component {
+    componentDidMount() {
+        socket.on(`connection`, data => {
+            console.log('user connected');
+            console.log(data);
+        })
+    }
+
     render() {
         return (
             <h2>Hello World!</h2>
