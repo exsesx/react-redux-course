a4860956a4eb71e49ada7c4a5fe824aebbe72d5e
exsesx <exsesx@gmail.com>
2017-11-23 02:17:16 +0200

    Connected clients counter added to socket

--- react-redux-course-chat/server/socket.js
+++ react-redux-course-chat/server/socket.js
@@ -3,15 +3,20 @@ const server = require("./server");
 const io = require('socket.io')(server);
 const socketioJwt = require('socketio-jwt');
 
+let clients = [];
 io.sockets.on('connection', socketioJwt.authorize({
     secret: 'mySecretKey',
     callback: 15000
 })).on('authenticated', function (socket) {
+    clients.push(socket);
     socket.emit('Introduction', socket.decoded_token);
-    console.log('User ' + socket.decoded_token.username + ' has connected to the chat.');
+    console.log(socket.decoded_token.username + ' has connected to the chat.');
 
     socket.on('disconnect', function () {
-        console.log('User ' + socket.decoded_token.username + ' has left the chat.');
+        console.log(socket.decoded_token.username + ' has left the chat.');
+
+        let i = clients.indexOf(socket);
+        clients.splice(i, 1);
     });
 });
 
